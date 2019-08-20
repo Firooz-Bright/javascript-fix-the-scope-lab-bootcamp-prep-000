fddf1373ded6fe339a16146f2aeca02f0242bf89
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-20 04:29:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ var animal = 'cat'
 }
 
 function add2(n) {
-  return n + two
-
   // Feel free to move things around!
   const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {
@@ -28,3 +27,5 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction
+var theFunk = funkyFunction()
+theFunk = theFunk()
